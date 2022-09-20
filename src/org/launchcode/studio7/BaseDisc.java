9a48a8e9458a62f7e6fa5fcde00bdd640e7827e4
Alex <alexmdalton@sbcglobal.net>
2022-09-19 19:56:18 -0500

    Work from studio time

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,21 @@
+package org.launchcode.studio7;
+
+import java.util.HashMap;
+
+public abstract class BaseDisc {
+    String name;
+    double capacity;
+    HashMap<String,String> contents = new HashMap<>();
+    String discType;
+
+
+
+    void displayProperties(){
+
+    }
+
+    void skipChapter(){
+
+    }
+
+}
